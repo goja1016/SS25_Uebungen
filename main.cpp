d02a97fbce8a94e7f6a1cd2c2ecc1d7dd59ce6ac
goja1016 <jannis.goldnik@gmail.com>
2025-04-18 19:00:26 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Autovermietung.h"
+using namespace std;
+
+
+// Mietwagen erstellen
+int main() 
+{
+    Autovermietung vermietung;
+    vermietung.dialog();
+    return 0;
+}
